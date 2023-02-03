969b693e30341b2aaf7fb096d810bb356d4cfde7
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-02-03 19:25:03 +0100

    merge

---
+++ src/main/java/net/chatfilter/chatfilterbackend/ChatFilterBackendApplication.java
@@ -0,0 +1,13 @@
+package net.chatfilter.chatfilterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatFilterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatFilterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/chatfilter/chatfilterbackend/ChatFilterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.chatfilter.chatfilterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatFilterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
