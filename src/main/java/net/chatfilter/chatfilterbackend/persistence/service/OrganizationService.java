6bfa26c08c5fe50be6f5a2bc2beeb2ba85bcada5
Alex Castro <35381285+alexcastro1919@users.noreply.github.com>
2023-02-08 21:06:29 +0100

    Add organization service

---
+++ src/main/java/net/chatfilter/chatfilterbackend/persistence/service/OrganizationService.java
@@ -0,0 +1,31 @@
+package net.chatfilter.chatfilterbackend.persistence.service;
+
+import net.chatfilter.chatfilterbackend.persistence.entity.organization.Organization;
+import net.chatfilter.chatfilterbackend.persistence.repository.OrganizationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrganizationService {
+
+    @Autowired
+    private OrganizationRepository repository;
+
+    public void create(Organization organization) {
+        repository.insert(organization);
+    }
+
+    public Organization getByUUID(UUID uuid) {
+        return repository.findById(uuid).orElse(null);
+    }
+
+    public void update(Organization organization) {
+        repository.save(organization);
+    }
+
+    public void delete(UUID uuid) {
+        repository.deleteById(uuid);
+    }
+}
